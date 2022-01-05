16be85045c2a0379b72adceffa47f9808f35d247
Fahad Ahmed <fahad.ahmed@spark.co.nz>
2022-01-05 12:22:50 +1100

    created a menu section that includes filters and show more button

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import Posts from './data/posts.json';
 
 const PRODUCTS_PER_PAGE = 9;
 const TOTAL_PRODUCTS = Posts.data.length;
+const PRODUCT_TYPE = ['all','boots', 'gumshoes', 'sandals', 'shoes', 'slippers', 'sneakers'];
+const SEX = ['all', 'man', 'woman'];
+const COLOR = ['all','beige', 'black', 'blue', 'brown', 'red', 'white'];
+
 export default function App() {
 
-  console.log(TOTAL_PRODUCTS);
   const [products, setProducts] = useState(Posts.data);
   const [productsCount, setProductsCount] = useState(PRODUCTS_PER_PAGE);
   const [totalFilterProductsCount, setTotalFilterProductsCount] = useState(TOTAL_PRODUCTS);
@@ -20,7 +23,6 @@ export default function App() {
   }, [productsCount])
 
   const handlePagination = () => {
-    console.log('We need to update the products page count');
     setProductsCount(productsCount + PRODUCTS_PER_PAGE);
   }
 
@@ -28,8 +30,27 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <div>{`showing ${productsCount} of ${totalFilterProductsCount}`}</div>
-      <button type="button" onClick={handlePagination}>Load More</button>
+      <div className="menu">
+        <div>
+          <select name="" id="">
+            {PRODUCT_TYPE.map((productType, i) => (
+              <option value={productType} key={i}>{productType}</option>
+            ))}
+          </select>
+          <select name="" id="">
+            {SEX.map(gender => (
+              <option value={gender} key={gender}>{gender}</option>
+            ))}
+          </select>
+          <select name="" id="">
+            {COLOR.map(colour => (
+              <option value={colour} key={colour}>{colour}</option>
+            ))}
+          </select>
+        </div>
+        <div>{`showing ${productsCount} of ${totalFilterProductsCount}`}</div>
+        <button type="button" onClick={handlePagination}>Load More</button>
+      </div>
       <div className="container">{products.map((product, i) => {
         return (
           <div key={i} className="product">{product.color} - {product.sex} - {product.type}</div>
